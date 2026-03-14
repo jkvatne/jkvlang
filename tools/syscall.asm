91fc077ddef3315bfc2e2dd373d8831a6dcc9114
Jan Kåre Vatne <jkvatne@online.no>
2026-03-14 13:04:57 +0100

    Ok malloc/mfree in syscall.asm

--- tools/syscall.asm
+++ tools/syscall.asm
@@ -1,6 +1,46 @@
 section .text
 
 global syscall
+global malloc
+global mfree
+
+extern GetProcessHeap
+extern HeapAlloc
+extern HeapFree
+
+; malloc returns in rax a pointer to the allocated memory or null.
+; One argument is needed, in rax, and that is the requested size in bytes.
+; Returns the pointer in rax
+malloc:
+    push rbp                         ; Prologue: Save frame pointer
+    mov rbp, rsp                     ; Prologue: Setup new frame pointer.
+    and rsp, -16                     ; Align stack by clearing the 4 lsb
+    sub rsp, 32                      ; Reserve shadow space
+    mov rdi, rax                     ; Save size into rdi
+    call GetProcessHeap
+    mov rcx, rax                     ; Argument 1, Handle from GetProcessHeap moved into rcx
+    mov rdx, 8                       ; Arbument 2, Flags into rdx, 8 means allocated memory is zeroed
+    mov r8, rdi
+    call HeapAlloc
+    leave                            ; Epilogue: Restore old frame pointer
+    ret                              ; Epilogue: Return
+
+; free will free the memory pointed to by rax.
+; It assumes it is from the default Process Heap returned from GetProcessHeap
+; No return value.
+mfree:
+    push rbp
+    mov rbp, rsp
+    and rsp, -16                     ; Align stack by clearing the 4 lsb
+    sub rsp, 40                      ; Reserve shadow space
+    mov rdi, rax                     ; Save ptr in rdi
+    call GetProcessHeap
+    mov rcx, rax                     ; Argument 1, Handle from GetProcessHeap moved into rcx
+    mov rdx, 0                       ; Argument 2, flags into rdx, 0 must be used
+    mov r8, rdi                      ; Argument 3, move memory pointer into r8
+    call HeapFree
+    leave
+    ret
 
 
 ; syscall will call any dll function that is reachable
--- tools/test_assembler.asm
+++ tools/test_assembler.asm
@@ -15,10 +15,8 @@ section .text
 
 extern GetStdHandle                             ; Import external symbols
 extern WriteFile                                ; Windows API functions, not decorated
-extern GetStdHandle
 extern WriteFile
 extern ExitProcess
-extern HeapAlloc
 extern printf
 extern GetCommandLineA
 extern GetCommandLineW
@@ -27,12 +25,12 @@ extern GetEnvironmentStringsW
 extern CreateFileA
 extern CreateFileW
 extern CloseHandle
-extern GetProcessHeap
 extern syscall                                ; fraom syscall.asm
+extern malloc
+extern mfree
 
 global _start                                 ; Export symbols. The entry point
-global _alloc
-global _assert
+global assert
 
 section .data                                   ; Initialized data segment
     message         db "Message from WriteFile", 0Dh, 0Ah
@@ -42,7 +40,8 @@ section .data                                   ; Initialized data segment
     test5par        db "Should be numbers 2-5 here: %d, %d, %d, %d", 0Dh, 0Ah, 00h
     test6par        db "Should be numbers 2-6 here: %d, %d, %d, %d, %d", 0Dh, 0Ah, 00h
     test7par        db "Should be numbers 2-7 here: %d, %d, %d, %d, %d, %d", 0Dh, 0Ah, 00h
-    spmess          db "... rsp = 0x%X", 0Dh, 0Ah, 00h
+    axmess          db "... rax = 0x%X", 0Dh, 0Ah, 00h
+    ptrmess         db "... ptr = 0x%X", 0Dh, 0Ah, 00h
 
 section .bss                                    ; Uninitialized data segment
 
@@ -50,33 +49,27 @@ alignb 8
     StdOutputHandle resq 1
     StdErrorHandle  resq 1
     StdInputHandle  resq 1
-    Written         resq 1
+    written         resq 1
+    heap            resq 1
+    readback        resq 1
 
 section .text
 
 ; Assert will print error message if [sp] is zero (false)
 _assert:
 
-; HeapAlloc(Handle,Flags,Size), using Microsoft ABI
-_alloc:
-    push rbp
-    mov rbp, rsp
-    and rsp, -16                        ; Align stack by clearing the 4 lsb
-    sub rsp, 32                         ; Reserve shadow space
-    call GetProcessHeap
-    mov rcx, rax                     ; Handle into rcx
-    mov rdx, 8                       ; Flags into rdx
-    mov r8, 32                       ; Size into r8
-    sub rsp, 48                      ;
-    call HeapAlloc
-    add rsp, 48
-    mov rsp, rbp
-    pop rbp
+printax:
+    push axmess
+    push rax
+    mov rax, 2
+    mov rdi, printf
+    call syscall
+    add sp, 8*2
     ret
 
-printsp:
-    push spmess
-    push rsp
+printptr:
+    push ptrmess
+    push rax ; qword [heap]
     mov rax, 2
     mov rdi, printf
     call syscall
@@ -86,7 +79,8 @@ printsp:
 _start:
     sub   rsp, 40                                  ; Align the stack to a multiple of 16 bytes+32 bytes shadow
 
-    call printsp
+    mov rax, rsp
+    call printax
 
     ; Print a startup message with integer parameters using the prinf from msvcrt.dll
     ; Must link with msvcrt.dll
@@ -96,7 +90,8 @@ _start:
     mov r9,  1            ; Forth argument: number
     call printf           ; Call printf
 
-    call printsp
+    mov rax, rsp
+    call printax
 
     mov   ecx, STD_OUTPUT_HANDLE
     call  GetStdHandle
@@ -110,18 +105,20 @@ _start:
     call  GetStdHandle
     mov   qword [rel StdInputHandle], RAX
 
-    call printsp
+    mov rax, rsp
+    call printax
 
     ; Test using WriteFile
     sub   RSP, 16                                  ; 5th parameter + align stack to a multiple of 16 bytes
     mov   RCX, qword [StdOutputHandle]             ; 1st parameter is the handle
     lea   RDX, [message]                           ; 2nd parameter is a pointer to the text to be written
     mov   R8, messlen                              ; 3rd parameter is the number of bytes to write
-    lea   R9, [rel Written]                        ; 4th parameter is a pointer to the variable receiving the number of bytes written.
+    lea   R9, [rel written]                        ; 4th parameter is a pointer to the variable receiving the number of bytes written.
     mov   qword [RSP + 32], 0                      ; 5th parameter is a pointer to the lpOverlapped structure (or nil).
     call  WriteFile                                ; Call the WriteFile function found in kernel32.dll (must be linked to)
     add   RSP, 16
-    call printsp
+    mov rax, rsp
+    call printax
 
     ; Test using syscall
     push test4par              ; 1st parameter
@@ -132,8 +129,6 @@ _start:
     mov rdi, printf             ; Address to call
     call syscall
     add sp, 8*4
-
-    call printsp
 
     ; Test using syscall
     push test5par              ; 1st parameter
@@ -146,7 +141,8 @@ _start:
     call syscall
     add sp, 8*5
 
-    call printsp
+    mov rax, rsp
+    call printax
 
     ; Test using syscall
     push test6par              ; 1st parameter
@@ -160,7 +156,8 @@ _start:
     call syscall
     add sp, 8*6
 
-    call printsp
+    mov rax, rsp
+    call printax
 
     push test7par              ; 1st parameter
     push 2                      ; 2nd parameter
@@ -174,7 +171,25 @@ _start:
     call syscall
     add sp, 8*7
 
-    call printsp
+    mov rax, rsp
+    call printax
+
+    mov rax, 4096
+    call malloc
+    mov qword [heap], rax
+    call printptr
+
+    ; Store value to heap
+    mov rdi, [heap]
+    mov qword [rdi], 0x123456
+    ; Read back from heap
+    mov rax, [rdi]
+    call printax
+
+    ; Test mfree. Should give rax=1 after call to mfree
+    mov rax, [heap]
+    call mfree
+    call printax
 
     ; Exit with error code 1
     mov   rcx, 123
