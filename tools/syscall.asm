f7c080e641920f4f6249b1791117bfff40d3f98a
Jan Kåre Vatne <jkvatne@online.no>
2026-03-16 10:29:03 +0100

    Add default message to assert

--- tools/syscall.asm
+++ tools/syscall.asm
@@ -19,18 +19,66 @@
 ; 14 r14   Preserved
 ; 15 r15   Preserved
 
+%define STD_INPUT_HANDLE -10
+%define STD_OUTPUT_HANDLE -11
+%define STD_ERROR_HANDLE -12
 
-section .text
+section .data
+    assert_failed  db "Assert failed",0Dh, 0Ah, 00h
 
+; Exported symbols from syscall.asm
 global syscall
 global malloc
 global mfree
 global assert
+global exit
+global sysinit
 
+; Symbols from kernel32
+extern ExitProcess
 extern GetProcessHeap
 extern HeapAlloc
 extern HeapFree
+extern GetStdHandle
 
+; Symbols from msvcrt.dll
+extern printf
+
+section .bss
+alignb 8
+    StdOutputHandle resq 1
+    StdErrorHandle  resq 1
+    StdInputHandle  resq 1
+    written         resq 1
+
+section .text
+
+writefile:
+    ; Test using WriteFile
+    sub   RSP, 16                                  ; 5th parameter + align stack to a multiple of 16 bytes
+    mov   RCX, qword [StdOutputHandle]             ; 1st parameter is the handle
+    ;lea   RDX, [message]                           ; 2nd parameter is a pointer to the text to be written
+    ;mov   R8, messlen                              ; 3rd parameter is the number of bytes to write
+    lea   R9, [rel written]                        ; 4th parameter is a pointer to the variable receiving the number of bytes written.
+    mov   qword [RSP + 32], 0                      ; 5th parameter is a pointer to the lpOverlapped structure (or nil).
+    ;call  WriteFile                                ; Call the WriteFile function found in kernel32.dll (must be linked to)
+    add   RSP, 16
+    ret
+
+; assert will verify that the first arbument is true (not 0)
+; if ax is null, it will print an error message using printf,
+; with optional additional parameters.
+; The stack will contain <bool><messageptr><arg1><arg2>..
+; rbx should contain the (number of arguments) * 8.
+assert:
+    mov rax, [rsp+rbx]      ; Load the bool argument to be tested
+    or rax, rax             ; Set flags
+    jz L1                   ; Jump if the bool argument fwas false
+    ret                     ; Returns if assert(true)
+L1: sub rbx, 8              ; Remove the first (bool) argument
+    mov rcx, assert_failed  ; Load default error message if assert has no message
+    mov rdi, printf         ; Call the printf function in msvcrt.dll
+    jmp syscall
 
 ; malloc returns in rax a pointer to the allocated memory or null.
 ; One argument is needed, in rax, and that is the requested size in bytes.
@@ -76,6 +124,9 @@ syscall:
     and rsp, -16          ; Align stack by clearing the 4 lsb
     sub rsp, 80           ; Reserve space for arguments to the called function
 
+    or rbx, rbx
+    jz docall
+
     mov rcx, [rbx+rbp+8]    ; cx = First argument: format string
     sub rbx, 8
     jc docall
@@ -115,3 +166,38 @@ docall:
     call [rdi]
     leave
     ret
+
+; exit have one parameter - the error code, found in ax
+exit:
+    push rbp                         ; Prologue: Save frame pointer
+    mov rbp, rsp                     ; Prologue: Setup new frame pointer.
+    and rsp, -16                     ; Align stack by clearing the 4 lsb
+    sub rsp, 32                      ; Reserve shadow space
+    mov rcx, rax
+    call ExitProcess
+    leave
+    ret
+
+; sysinit will initialize the console handles
+sysinit:
+    push rbp                         ; Prologue: Save frame pointer
+    mov rbp, rsp                     ; Prologue: Setup new frame pointer.
+    and rsp, -16                     ; Align stack by clearing the 4 lsb
+    sub rsp, 32                      ; Reserve shadow space
+
+    ; Load the handle for standard output
+    mov   ecx, STD_OUTPUT_HANDLE
+    call  GetStdHandle
+    mov   [rel StdOutputHandle], rax
+
+    mov   ecx, STD_ERROR_HANDLE
+    call  GetStdHandle
+    mov   [rel StdErrorHandle], rax
+
+    mov   ecx, STD_INPUT_HANDLE
+    call  GetStdHandle
+    mov   [rel StdInputHandle], rax
+
+    leave
+    ret
+
--- tools/test_assembler.asm
+++ tools/test_assembler.asm
@@ -3,32 +3,20 @@
 ; This is a file used to verify the assmembler setup and to test 
 ; calling system files.
 
-%define STD_INPUT_HANDLE -10
-%define STD_OUTPUT_HANDLE -11
-%define STD_ERROR_HANDLE -12
 %define false 0
 %define true  1
 
-section .text
-
-extern GetStdHandle                             ; Import external symbols
-extern WriteFile                                ; Windows API functions, not decorated
-extern WriteFile
-extern ExitProcess
-extern printf
-extern GetCommandLineA
-extern GetCommandLineW
-extern GetEnvironmentStringsA
-extern GetEnvironmentStringsW
-extern CreateFileA
-extern CreateFileW
-extern CloseHandle
+; Symbols imported from syscall.asm
 extern syscall                                ; fraom syscall.asm
 extern malloc
 extern mfree
+extern assert
+extern exit
+extern printf
+extern sysinit
+extern ExitProcess
 
 global _start                                 ; Export symbols. The entry point
-global assert
 
 section .data                                   ; Initialized data segment
     message         db "Message from WriteFile", 0Dh, 0Ah
@@ -40,23 +28,18 @@ section .data                                   ; Initialized data segment
     test7par        db "Should be numbers 2-7 here: %d, %d, %d, %d, %d, %d", 0Dh, 0Ah, 00h
     axmess          db "... rax = 0x%X", 0Dh, 0Ah, 00h
     printbxmess     db "... rbx = 0x%X", 0Dh, 0Ah, 00h
-    ptrmess         db "... ptr = 0x%X", 0Dh, 0Ah, 00h
     sp_mess         db "...  sp = 0x%X", 0Dh, 0Ah, 00h
     assert_true_mess   db "==== Assert true message, x=%d",0Dh, 0Ah, 00h
     assert_false_mess  db "==== Assert false message, x=%d",0Dh, 0Ah, 00h
-    assert_args_mess  db "==== Assert false message, x=%d, y=%d",0Dh, 0Ah, 00h
+    assert_args_mess  db "==== Assert false with two arguments, x=%d, y=%d",0Dh, 0Ah, 00h
+
 section .bss                                    ; Uninitialized data segment
 
 alignb 8
-    StdOutputHandle resq 1
-    StdErrorHandle  resq 1
-    StdInputHandle  resq 1
-    written         resq 1
     heap            resq 1
     readback        resq 1
 
 section .text
-
 
 printax:
     push axmess
@@ -100,29 +83,7 @@ _start:
 
     call print_sp
 
-    mov   ecx, STD_OUTPUT_HANDLE
-    call  GetStdHandle
-    mov   qword [rel StdOutputHandle], RAX
-
-    mov   ecx, STD_ERROR_HANDLE
-    call  GetStdHandle
-    mov   qword [rel StdErrorHandle], RAX
-
-    mov   ecx, STD_INPUT_HANDLE
-    call  GetStdHandle
-    mov   qword [rel StdInputHandle], RAX
-
-    call print_sp
-
-    ; Test using WriteFile
-    sub   RSP, 16                                  ; 5th parameter + align stack to a multiple of 16 bytes
-    mov   RCX, qword [StdOutputHandle]             ; 1st parameter is the handle
-    lea   RDX, [message]                           ; 2nd parameter is a pointer to the text to be written
-    mov   R8, messlen                              ; 3rd parameter is the number of bytes to write
-    lea   R9, [rel written]                        ; 4th parameter is a pointer to the variable receiving the number of bytes written.
-    mov   qword [RSP + 32], 0                      ; 5th parameter is a pointer to the lpOverlapped structure (or nil).
-    call  WriteFile                                ; Call the WriteFile function found in kernel32.dll (must be linked to)
-    add   RSP, 16
+    call sysinit
 
     call print_sp
 
@@ -181,7 +142,7 @@ _start:
 
     mov rax, 4096
     call malloc
-    mov qword [heap], rax
+    mov [heap], rax
 
     ; Store value to heap
     mov rdi, [heap]
@@ -226,20 +187,12 @@ _start:
     call assert
     add sp, 4*8
 
-    ; Exit with error code 1
-    mov   rcx, 1234
-    call  ExitProcess
+    ; Test assert with one arguments (no message)
+    push false
+    mov rbx, 8
+    call assert
+    add sp, 8
 
-; assert will verify that the first arbument is true (not 0)
-; if ax is null, it will print an error message using printf,
-; with optional additional parameters.
-; The stack will contain <bool><messageptr><arg1><arg2>..
-; rbx should contain the (number of arguments) * 8.
-assert:
-    mov rax, [rsp+rbx]    ; Load the bool argument to be tested
-    or rax, rax           ; Set flags
-    jz L1                 ; Jump if the bool argument fwas false
-    ret                   ; Returns if assert(true)
-L1: sub rbx, 8            ; Remove the first (bool) argument
-    mov rdi, printf       ; Call the printf function in msvcrt.dll
-    jmp syscall
+    ; Exit with error code 1
+    mov   rax, 1234
+    call  exit
