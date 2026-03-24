a952e0c68751d8c378bd66e2c7b5388ebefafa70
Jan Kåre Vatne <jkvatne@online.no>
2026-03-24 09:59:34 +0100

    Testing sys.asm. Had to rename malloc and mfree

--- lib/sys.asm
+++ lib/sys.asm
@@ -154,3 +154,112 @@ _L3:
     call [rdi]
     leave
     ret
+
+
+; assert will verify that the first arbument (rax) is true (not 0)
+; with optional additional parameters.
+; The stack will contain <messageptr><arg1><arg2>..
+; rbx should contain the size of the stack. (number of arguments-1) * 8.
+; rax is already the value to be tested
+; NB: Assert will append CRLF after the message.
+assert:
+    push rbp
+    mov rbp, rsp          ; Setup new frame pointer
+    and rsp, -16          ; Align stack by clearing the 4 lsb
+    sub rsp, 96           ; Reserve space for arguments to the called function
+
+    or rax, rax           ; Set z-flag if rax is zero
+    jz _L1                 ; Jump if the bool argument was false
+    leave
+    ret                   ; Returns if assert(true)
+_L1:
+    mov rcx, [rbp+16]    ; rcx = First argument: format string
+    add rcx, 4
+    sub rbx, 8
+    or rbx, rbx
+    jz _L2
+
+    mov rdx, [rbp+24]    ; dx = Second argument
+    sub rbx, 8
+    jc _L2
+
+    mov r8,  [rbp+32]    ; r8 = Third argument
+    sub rbx, 8
+    jc _L2
+
+    mov r9,  [rbp+40]    ; r9 = Forth argument
+    sub rbx, 8
+    jc _L2
+
+    mov rsi, [rbp+48]    ; Fifth argument onto stack
+    mov [rsp+32], rsi
+    sub rbx, 8
+    jc _L2
+
+    mov rsi, [rbp+56]
+    mov [rsp+40], rsi     ; Sixth argument onto stack
+    sub rbx, 8
+    jc _L2
+
+    mov rsi, [rbp+64]
+    mov [rsp+48], rsi     ; Seventh argument onto stack
+    sub rbx, 8
+    jc _L2
+
+    mov rsi, [rbp+72]
+    mov [rsp+56], rsi     ; Eight argument onto stack
+    sub rbx, 8
+    jc _L2
+
+    mov rsi, [rbp+80]
+    mov [rsp+64], rsi     ; Nineth argument onto stack
+    sub rbx, 8
+    jc _L2
+
+    mov rsi, [rbp+88]
+    mov [rsp+72], rsi     ; Tenth argument onto stack
+    jmp _L2
+
+_L2:
+    call printf
+
+    mov rcx, crlf_str
+    call printf
+
+    leave
+    ret
+
+
+; malloc returns in rax a pointer to the allocated memory or null.
+; One argument is needed, in rax, and that is the requested size in bytes.
+; Returns the pointer in rax
+_alloc:
+    push rbp                         ; Prologue: Save frame pointer
+    mov rbp, rsp                     ; Prologue: Setup new frame pointer.
+    and rsp, -16                     ; Align stack by clearing the 4 lsb
+    sub rsp, 32                      ; Reserve shadow space
+    mov rdi, rax                     ; Save size into rdi
+    call GetProcessHeap
+    mov rcx, rax                     ; Argument 1, Handle from GetProcessHeap moved into rcx
+    mov rdx, 8                       ; Arbument 2, Flags into rdx, 8 means allocated memory is zeroed
+    mov r8, rdi
+    call HeapAlloc
+    leave                            ; Epilogue: Restore old frame pointer
+    ret                              ; Epilogue: Return
+
+; free will free the memory pointed to by rax.
+; It assumes it is from the default Process Heap returned from GetProcessHeap
+; No return value.
+_free:
+    push rbp
+    mov rbp, rsp
+    and rsp, -16                     ; Align stack by clearing the 4 lsb
+    sub rsp, 40                      ; Reserve shadow space
+    mov rdi, rax                     ; Save ptr in rdi
+    call GetProcessHeap
+    mov rcx, rax                     ; Argument 1, Handle from GetProcessHeap moved into rcx
+    mov rdx, 0                       ; Argument 2, flags into rdx, 0 must be used
+    mov r8, rdi                      ; Argument 3, move memory pointer into r8
+    call HeapFree
+    leave
+    ret
