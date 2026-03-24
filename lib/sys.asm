eafcbad366064dc6660fc271836c093087329e75
Jan Kåre Vatne <jkvatne@online.no>
2026-03-24 09:55:17 +0100

    Testing sys.asm. Starting small

---
+++ lib/sys.asm
@@ -0,0 +1,87 @@
+
+global main
+global malloc
+global mfree
+global assert
+global exit
+global sysinit
+global StdOutputHandle
+global StdErrorHandle
+global StdInputHandle
+global error
+global get_win_error
+global print
+
+
+; Symbols from kernel32
+extern ExitProcess
+extern GetProcessHeap
+extern HeapAlloc
+extern HeapFree
+extern GetStdHandle
+extern GetLastError
+extern FormatMessageA
+
+; Symbols from msvcrt.dll
+extern printf
+%define STD_INPUT_HANDLE  -10
+%define STD_OUTPUT_HANDLE -11
+%define STD_ERROR_HANDLE  -12
+%define MAX_ERROR_LEN     40*8
+
+section .rodata
+    crlf_str  db 0Dh, 0Ah, 00h
+
+section .bss
+    alignb 8
+    StdOutputHandle resq 1
+    StdErrorHandle  resq 1
+    StdInputHandle  resq 1
+    error_len       resq 1              ; 16 bit string length
+    error           resq MAX_ERROR_LEN
+
+
+section .text
+sysinit:
+    ; sysinit will initialize the console handles
+    push rbp                         ; Prologue: Save frame pointer
+    mov rbp, rsp                     ; Prologue: Setup new frame pointer.
+    and rsp, -16                     ; Align stack by clearing the 4 lsb
+    sub rsp, 32                      ; Reserve shadow space
+
+    ; Load the handle for standard output
+    mov   ecx, STD_OUTPUT_HANDLE
+    call  GetStdHandle
+    mov   [rel StdOutputHandle], rax
+
+    mov   ecx, STD_ERROR_HANDLE
+    call  GetStdHandle
+    mov   [rel StdErrorHandle], rax
+
+    mov   ecx, STD_INPUT_HANDLE
+    call  GetStdHandle
+    mov   [rel StdInputHandle], rax
+
+    mov qword [error], 0
+    mov [error_len], word 0
+    leave   
+    ret
+    
+main:       
+   mov rbp, rsp; for correct debugging
+   ; call sysinit
+   
+   xor rax, rax                     ; Error code = 0
+   ret
+   
+
+; exit have one parameter - the error code, found in rax
+exit:
+    push rbp                         ; Prologue: Save frame pointer
+    mov rbp, rsp                     ; Prologue: Setup new frame pointer.
+    and rsp, -16                     ; Align stack by clearing the 4 lsb
+    sub rsp, 32                      ; Reserve shadow space
+    mov rcx, rax
+    call ExitProcess
+    leave
+    ret   
